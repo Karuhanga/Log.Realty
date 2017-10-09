c668ada13c2a5c3f54bb0b34fb1a6d974054aba1
Karuhanga Lincoln <karuhanga475@gmail.com>
2017-10-09 23:27:09 +0300

    Added further todos

--- Log.Realty/app/src/main/java/ug/karuhanga/logrealty/Activities/Details.java
+++ Log.Realty/app/src/main/java/ug/karuhanga/logrealty/Activities/Details.java
@@ -20,6 +20,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.orm.query.Select;
 
@@ -124,29 +125,6 @@ public class Details extends AppCompatActivity implements DetailedLocation.OnFra
 
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_detailed_locations, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @Override
     public void onFragmentInteraction(Uri uri) {
 
@@ -183,7 +161,6 @@ public class Details extends AppCompatActivity implements DetailedLocation.OnFra
                 case Helpers.FRAGMENT_LOCATIONS: return DetailedLocation.newInstance(ids.get(position));
                 default: return DetailedLocation.newInstance(ids.get(position));
             }
-
         }
 
         @Override
--- Log.Realty/app/src/main/java/ug/karuhanga/logrealty/Fragments/DetailedLocation.java
+++ Log.Realty/app/src/main/java/ug/karuhanga/logrealty/Fragments/DetailedLocation.java
@@ -3,11 +3,24 @@ package ug.karuhanga.logrealty.Fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import com.orm.query.Select;
+
+import java.util.HashMap;
+
+import ug.karuhanga.logrealty.Data.Location;
+import ug.karuhanga.logrealty.Helpers;
 import ug.karuhanga.logrealty.R;
 
 /**
@@ -18,10 +31,23 @@ import ug.karuhanga.logrealty.R;
  * Use the {@link DetailedLocation#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DetailedLocation extends Fragment {
+public class DetailedLocation extends Fragment implements TextWatcher, View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
 
     private Long location;
+    private ImageButton buttonLocation;
+    private ImageButton buttonRent;
+    private EditText editTextLocation;
+    private EditText editTextRent;
+    private TextView textViewLocation;
+    private TextView textViewRent;
+    private FloatingActionButton fab;
+
+    private int previousColor;
+    private int editCount;
+    private Location locationObject;
+
+    private HashMap<String, Boolean> editting= new HashMap<>();
 
     private OnFragmentInteractionListener mListener;
 
@@ -51,13 +77,37 @@ public class DetailedLocation extends Fragment {
         if (getArguments() != null) {
             location = getArguments().getLong("id");
         }
+        editting.put("location", false);
+        editting.put("rent", false);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detailed_location, container, false);
+        View view= inflater.inflate(R.layout.detailed_location_fragment, container, false);
+        buttonLocation= view.findViewById(R.id.button_location_fragment_detailed_location);
+        buttonRent= view.findViewById(R.id.button_amount_fragment_detailed_location);
+        textViewLocation= view.findViewById(R.id.text_view_location_fragment_detailed_location);
+        textViewRent= view.findViewById(R.id.text_view_amount_fragment_detailed_location);
+        editTextLocation= view.findViewById(R.id.edit_text_location_fragment_detailed_location);
+        editTextRent= view.findViewById(R.id.edit_text_amount_fragment_detailed_location);
+        fab= view.findViewById(R.id.fab_detailed_location);
+
+        previousColor= editTextLocation.getCurrentTextColor();
+        editCount= 0;
+        locationObject= Location.findById(Location.class, location);
+
+        textViewLocation.setText(locationObject.getName());
+        textViewRent.setText(Helpers.toCurrency(locationObject.getDefaultRent()));
+        editTextLocation.setText(textViewLocation.getText());
+        textViewRent.setText(textViewRent.getText());
+
+        buttonLocation.setOnClickListener(this);
+        buttonRent.setOnClickListener(this);
+        fab.setOnClickListener(this);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -82,6 +132,96 @@ public class DetailedLocation extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view==fab){
+            if (editCount==0){
+                if (buttonLocation.getVisibility()==View.VISIBLE){
+                    fab.setImageResource(R.drawable.icon_edit);
+                    buttonLocation.setVisibility(View.INVISIBLE);
+                    buttonRent.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    fab.setImageResource(R.drawable.ic_delete_black_24dp);
+                    buttonLocation.setVisibility(View.VISIBLE);
+                    buttonRent.setVisibility(View.VISIBLE);
+                }
+                return;
+            }
+            completeEdit();
+            return;
+        }
+        if (view==buttonLocation){
+            if (editting.get("location")){
+                editCount--;
+                editTextLocation.setVisibility(View.GONE);
+                textViewLocation.setVisibility(View.VISIBLE);
+                editTextLocation.setText(textViewLocation.getText());
+                buttonLocation.setImageResource(R.drawable.icon_edit);
+                editting.remove("location");
+                editting.put("location", false);
+                if (editCount==0){
+                    fab.setImageResource(R.drawable.ic_delete_black_24dp);
+                }
+                return;
+            }
+            editCount++;
+            if (editCount==1){
+                fab.setImageResource(R.drawable.ic_check_black_24dp);
+            }
+            textViewLocation.setVisibility(View.GONE);
+            editTextLocation.setVisibility(View.VISIBLE);
+            buttonLocation.setImageResource(R.drawable.ic_delete_black_24dp);
+            editting.remove("location");
+            editting.put("location", true);
+            return;
+        }
+
+        if (view==buttonRent){
+            if (editting.get("rent")){
+                editCount--;
+                editTextRent.setVisibility(View.GONE);
+                textViewRent.setVisibility(View.VISIBLE);
+                editTextRent.setText(textViewRent.getText());
+                buttonRent.setImageResource(R.drawable.icon_edit);
+                editting.remove("rent");
+                editting.put("rent", false);
+                if (editCount==0){
+                    fab.setImageResource(R.drawable.ic_delete_black_24dp);
+                }
+                return;
+            }
+            editCount++;
+            if (editCount==1){
+                fab.setImageResource(R.drawable.ic_check_black_24dp);
+            }
+            textViewRent.setVisibility(View.GONE);
+            editTextRent.setVisibility(View.VISIBLE);
+            buttonRent.setImageResource(R.drawable.ic_delete_black_24dp);
+            editting.remove("rent");
+            editting.put("rent", true);
+            return;
+        }
+    }
+
+    private void completeEdit() {
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
     }
 
     /**
